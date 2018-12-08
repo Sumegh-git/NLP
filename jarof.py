4a17867781ded575a63a82fbbe8c3583dc3cbc48
Sumegh-git <sumeghtech@gmail.com>
2018-12-08 15:00:28 +0530

    Updated

--- jarof.py
+++ jarof.py
@@ -1,17 +1,18 @@
+
 from multiprocessing import Process, Lock
 import jellyfish as jf
 import time
-
+#x="SakawISatamAtramtuprayARebrahmavAdinAm"
 global mx,ctr,st,ans,dictt
 mx=0
 dictt={}
 it= open("truth.txt",'r')
-ctr=0
+ct=0
 for line in it:
 	line=line.rstrip('\n')
-        dictt[line]=ctr
-	ctr+=1
-ctr=0
+        dictt[line]=ct
+	ct+=1
+#ctr=0
 it.close()
 it=open("truth.txt",'r')
 
@@ -20,31 +21,38 @@ st=""
 #with open("modslok.txt") as mf:
 # rd=[next(mf) for x in xrange(3)] 
 #print(rd)
-wr= open('linenos.txt','a')
+#wr= open('linenos.txt','a')
 ans=""
 start_time=time.time()
 
-def f(l,ss,es,ctr):
- l.acquire()	
+def f(ss,es,ctr,lctr):
+ wr=open(ctr,'w')
+ #l.acquire()	
  mx=0
 
  st=""
  rd=[]	
  #for inp in rds:
- with open("modslok.txt") as inp:
+ with open("mods2.txt") as inp:
  	for x1 in xrange(2*ss):
- 		next(inp)
- 	rd=[next(inp) for xx in xrange(2*(es-ss)+1)]	
-
+ 	    try:
+         	next(inp)
+            except StopIteration:
+                break
+ 	for xx in xrange(2*(es-ss)+1):
+           try: 
+            rd.append(next(inp))	
+           except StopIteration:
+            break
  
  for line in rd:
     if line == '\n':
       continue
-    wr.write(str(ctr))
+    wr.write(str(lctr))
     wr.flush()
     wr.write(' ')
     wr.flush()
-    ctr+=1    
+    lctr+=1    
     for c in line:
          if ord(c)==32:
            continue 
@@ -76,24 +84,27 @@ def f(l,ss,es,ctr):
 		    st+=c
     wr.write('\n')
     wr.flush()	
- l.release()	    
-
+ #l.release()	    
+ wr.close()
 if __name__ == '__main__':
 	#wr=open("linenos.txt",'a')
-        lock= Lock()
- 
+        #lock= Lock()
+        ii=0
+        lctr=0
 	for lp in xrange(0,18288,381):
-                ctr=lp
+                ctr='storg'+str(ii);
+                lctr+=381
+                ii+=1
                 #wr=open("linenos.txt",'a')
              
-		Process(target=f, args=(lock,lp,lp+380,ctr)).start()
+		Process(target=f, args=(lp,lp+380,ctr,lctr)).start()
                
               
         print("---%s seconds"%(time.time()-start_time))		
         
 it.close()
 #rdf.close()
-wr.close()
+#wr.close()
 		
 
 
